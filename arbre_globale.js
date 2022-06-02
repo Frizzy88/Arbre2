5608a1dbbc3d5093d353057bf69fe725001b1fea
Frantz J <92149661+Frizzy88@users.noreply.github.com>
2022-06-02 09:10:36 +0200

    Add files via upload

---
+++ arbre_globale.js
@@ -0,0 +1,421 @@
+
+const tab = {
+    "BIDJECK": {
+        "NJOCK BIDJECK": {
+            "YAP NDJOCK": {
+                "YAP": null,
+                "NDJOCK YAP": null
+            },
+            "NDJOCK Thomas": null,
+            "NDJOCK Sakeo": {
+                "BASSINGHA Eugene": {
+                    "NGO BASSINGHA Rose": null,
+                    "BASSINGHA Irene": null,
+                    "MBEM Rene": null,
+                    "EOG Simon": null,
+                    "NDJOCK Sakeo II": null,
+                    "NGO BASSINGHA": null,
+                    "NGO BASSINGHA Maria": null,
+                    "JOB LI BASSINGHA Jean Paul": null
+                },
+                "NYEMECK Pierre": null
+            },
+            "NDJOCK": null,
+            "BIDJECK": {
+                "BALOG": {
+                    "JOB LI BALOG": null,
+                    "NDJOCK BALOG": null
+                }
+            },
+            "NYEMECK NDJOCK Philippe": {
+                "NGO YO'O Jeanne": "NYEMECK Philippe"
+            }
+        },
+        "JOB LI BIDJECK": {
+            "BAHIDA BA JOB": {
+                "NGO BAHIDA Sapheria": {
+                    "NGO BAPA Madeleine": "BAPA Rodrigue",
+                    "NGO BAPA Honorine": {
+                        "NGO MBOG Marie Thérèse": null,
+                        "MBOG Michel Frédéric": null,
+                        "DOOH Pierre Pascal": null
+                    },
+                    "NGO BAPA Catherine": {
+                        "NDONGO Guy Cédric": null,
+                        "NDONGO Jana Francine Laure": "Chloé"
+                    },
+                    "BAPA Raymond": {
+                        "BAPA Talia Simone": null,
+                        "BAPA Paul harold": null
+                    },
+                    "BAPA Théophile": null,
+                },
+                "MBELECK BAHIDA JOB Daniel Sosthène": {
+                    "JOB Déborah Mireille Chantal": {
+                        "JOB Yannick Aimé": {
+                            "JOB Eirine Peace Déborah": null,
+                            "JOB Ayden Chrismaëlys": null,
+                            "JOB NJOH Etienne Yannick": null
+                        },
+                        "JOB Marie Johanna C.": {
+                            "Alisha": null,
+                            "Emeraude": null
+                        }
+                    },
+                    "JOB Joseph Théophile": null,
+                    "JOB BAHIDA Amy Marthe Florence": "JOB Marie-Ivana kAREN",
+                    "JOB Jean Calvin": null,
+                    "JOB Madeleine": null,
+                    "DJOB BAHIDA Jean Louis Roger - Emmanuel": {
+                        "JOB Marie-Lucresse Alexandra": "Une_Fille",
+                        "JOB Daniel Frank Junior": null
+                    }
+                },
+                "BAHIDA NGUENE Joseph": {
+                    "NGO NGUENE Marthe": null,
+                    "HOT NGUENE Antoine Depadoux": null,
+                    "NYOBE NGUENE Pierre Lotti": null,
+                    "NGO NGUENE Marie": null,
+                    "MAKON MA NGUENE Louis": null,
+                    "BIDJECK NGUENE Alain Richard": null,
+                    "NGO NGUENE Rose": null
+                },
+                "JOB BAHIDA Jean calvin": {
+                    "JOB Nicole": null,
+                    "JOB Jeanne Mélanie": "JOB BAHIDA Jean Calvin Stéphane",
+                    "JOB BAHIDA Emile Edgard": {
+                        "JOB Carson": null,
+                        "JOB Jacquelines Laeticia": null
+                    },
+                    "JOB Andre Alain Felix": {
+                        "JOB Jean Jules Audry": null,
+                        "JOB Agnès": null,
+                        "JOB Arthur": null,
+                        "JOB Henry": null
+                    },
+                    "JOB Henri Charles Timoléon": {
+                        "JOB Agnès Christelle": null,
+                        "JOB Charles Junior": null,
+                        "JOB Moïse Hermann": null,
+                        "JOB Charles Rebecca": null
+                    }
+                },
+                "NGO BAHIDA Marthe": {
+                    "NGO MANDENG": null,
+                    "NGO JOB": null,
+                    "BAHIDA Lydie": null,
+                    "DIWOUTA Daniel": {
+                        "DIWOUTA": null,
+                        "NGO BAHIDA Julienne": null,
+                        "BAHIDA Marthe Merline": null,
+                        "DIWOUTA Junior": null
+                    }
+                },
+                "BAHIDA Théodore": {
+                    "BAHIDA Philippe": {
+                        "BAHIDA Eugenie": null,
+                        "BAHIDA Reine Cornélie": null,
+                        "BAHIDA Jacky": null,
+                        "BAHIDA Cécile Riphade": null
+                    },
+                    "JOB Georges Raymond": {
+                        "NDJOCK Alice Joëlle": null,
+                        "BAHIDA Bahida": null,
+                        "BAHIDA Damas": null
+                    },
+                    "NGO JOB Flore Delphine": {
+                        "JOB Calvin Romuald": null,
+                        "NGO NDJOCK Marthe Julienne": null
+                    },
+                    "NGO BAHIDA Marthe Julienne": null,
+                    "JOB BAHIDA Jean Simon": {
+                        "JOB BAHIDA Theodore Simplice": null,
+                        "NGO BAHIDA Juliene Diane": null,
+                        "JOB BAHIDA Jean Calvin": null,
+                        "BAHIDA Idriss Raoul": "Jephté"
+                    }
+                }
+            },
+            "JOB LI JOB Jean": {
+                "JOB Mireille Rose": {
+                    "NJEM Marie Hélène": null,
+                    "NJEM Sarah Yolande": null,
+                    "JOB Rose Blanche": {
+                        "COTE Hughes Henri John": null,
+                        "BISSECK Guillaume Marie": null,
+                        "BISSECK Marie Hélène": null,
+                        "BISSECK Pierre Marie": null,
+                        "BISSECK Michel Philippe": null,
+                        "BISSECK Marc Henri": null
+                    },
+                    "JOB Jean Michel": {
+                        "JOB Isabelle Laure": {
+                            "FOULQUIER Anne-Marie": null,
+                            "FOULQUIER Zélie Heidi": null
+                        },
+                        "JOB Frantz Rudolf": null,
+                        "JOB Frédéric Charles": {
+                            "JOB Louis Ebwadou": null
+                        },
+                        "JOB Yves Henri Michel": {
+                            "JOB Erwan": null,
+                            "JOB Guewin": null
+                        },
+                        "JOB Jean Philippe": null,
+                        "JOB Patrick Bea": {
+                            "JOB Owen Kito": null,
+                            "JOB Neela": null,
+                            "JOB Aïdan": null
+                        },
+                        "JOB Nelly Cynthia": null,
+                        "JOB KAMGA Lima Isabelle": {
+                            "YAMSI JOB Gabe-Denzel Manaël": null
+                        }
+                    },
+                    "JOB Henri Pierre Nicole Alfred": {
+                        "JOB Yohan-Hélène": null,
+                        "JOB ian": null,
+                        "JOB Michel yohan Jing": {
+                            "une fille": null,
+                            "un garcon": null
+                        },
+                        "JOB Edwin John Maxwell": {
+                            "une fille": null
+                        },
+                        "JOB Fabien Yann Paul": null
+                    },
+                    "JOB Jean Charles Benjamin": {
+                        "JOB Laetitia": {
+                            "Dominique": null,
+                            "Lolwa": null,
+                            "Alexcendre": null
+                        },
+                        "JOB Natacha Blanche": null,
+                        "JOB Yolande Sally": {
+                            "JOB Elena Solange Crescence": null
+                        },
+                        "JOB Jean Charles, johnny": null,
+                        "JOB Elisabeth Malaïka Zélia": {
+                            "IMOMA James": null,
+                            "IMOMA Jason": null,
+                            "IMOMA Cloé": null
+                        },
+                        "JOB Kiera": null
+                    }
+                },
+                "Djôn Moïse": null,
+                "Ngo JOB Hermine": null,
+                "Ngo JOB Colette": {
+                    "JOB Marie Rose": null,
+                    "JOB Jean Claude": null
+                },
+                "JOB Jean Jules": {
+                    "JOB Gisèle Fidèle": {
+                        "NGO BAKONGO Emilie Arianne": "Kelia",
+                        "BAKONGO Simon Lionel": null,
+                        "NGO BAKONGO July Christelle": {
+                            "Léana": null,
+                            "Dayana": null
+                        },
+                        "NGO BAKONGO Céline Yolaine": null
+                    },
+                    "JOB Jean Marcel": {
+                        "JOB Maeva Ange Julie": null,
+                        "JOB Billy Jules": null
+                    },
+                    "JOB Marie Paule": null,
+                    "NGO DJOB JOB Angèle": {
+                        "JOB Njeba Alfred Lionel": {
+                            "Salvador": null,
+                            "Stephan": null
+                        },
+                        "JOB Stephan": null,
+                        "JOB Babi": null,
+                        "JOB X": null
+                    },
+                    "JOB Jean Pierre": null,
+                    "JOB Léopold": null,
+                    "JOB Joseph": null,
+                    "JOB Daniel": null
+                },
+                "Ngo JOB Alice": {
+                    "JOB MATIP": null,
+                    "NGO MATIP Laure Marie": null,
+                    "MATIP": null,
+                    "MATIP MA MATIP Elie": null,
+                    "NGO MATIP Mireille": null,
+                    "NGOS MATIP Jean": null
+                },
+                "JOB André Claude": {
+                    "JOB Annie": null,
+                    "JOB Yolande Nadine": {
+                        "Anré Jérémie": null,
+                        "William Phoenix": null
+                    },
+                    "JOB Irène Sophie": null,
+                    "JOB Eric Benoît": null,
+                    "JOB Corinne Marianne": null
+                },
+                "Ngo JOB Suzanne": {
+                    "NGO UM Marlysse": null,
+                    "BOUM OUM Pierre Henri": "6 enfants",
+                    "Minyem": null,
+                    "NGO UM Ruth": null,
+                    "NGO UM Solange": null,
+                    "NGO UM Sidonie": null,
+                    "BAHIDA Jean de Dieu": null,
+                    "JOB Alain André": null,
+                    "JOB Patrice": null
+                },
+                "Ngo JOB Agathe": {
+                    "TOUCK BAHEBEG Harry": null,
+                    "NDJOBA BAHEBEG Cécile": "BASSIME Julie Petra",
+                    "JOB BAHEBEG Martial": null,
+                    "BAHEBEG Marie": null,
+                    "BAHEBEG Fils Calvin": null
+                },
+                "JOB Jean Calvin": {
+                    "JOB Patrick Eric": null,
+                    "JOB Joseph Désiré": null,
+                    "JOB Jean Calvin II": null,
+                    "JOB Jean Olivier": null
+                },
+                "JOB Joseph": {
+                    "Ngo JOB Marie Esther": null,
+                    "NGUE Jean Sadrack": null,
+                    "Ngo JOB Lafortune": null,
+                    "JOB Jean Yves": null
+                },
+                "Ngo JOB Cathérine": "JOB Henri Serge",
+                "Ngo JOB Agnès": null,
+                "Ngo JOB Rose": null,
+                "Ngo JOB Simone": {
+                    "EONE Rigobert": null,
+                    "EONE René": null,
+                    "EONE JOB II": null,
+                    "NGO EONE Michèle": null,
+                    "EONE Maximilien": null
+                },
+                "NGO UM Hermine": null,
+                "YEBGA François": null,
+                "Ngo JOB Alice": null,
+                "JOB Jean Jacques": null,
+                "JOB Simon de Montfort": null
+            },
+            "NGO DJOB Naemi": {
+                "NGO NSUMB Rachel": "NGO TJOCK Mireille Naemi",
+                "NSUMB Amos": null,
+                "LIBOG LI LIBOG Jean Samuel": {
+                    "LIBOG Guillaume Augustin": null,
+                    "NGO LIBOG Marie Eléonore": {
+                        "NANA DJIKE Lea Julia Gailla": null,
+                        "NANA DJIKE daronne": null,
+                        "NANA DJIKE Samuel": null,
+                        "NANA DJIKE Madeleine": null
+                    },
+                    "NGO LIBOG Ernestine Céline": null,
+                    "NGO LIBOG Aline Rachèle": "JOB Jean Emile Lotti",
+                    "NGO LIBOG Jeanne Simone": null,
+                    "NGO LIBOG Madeleine Rose": null,
+                    "JOB LI LIBOG Jean Calvin 2": {
+                        "JOB LI LIBOG Celya Naemie": null,
+                        "JOB LI LIBOG Samuel Louis Curtis": null
+                    },
+                    "NGO LIBOG Mireille Naemi": null,
+                    "LIBOG Yvonne Elisabeth": "BASSOG Libyh Paul Ryan",
+                    "NGO LIBOG Evelyne Cécile": "LIBOG Félix Maxime Berthold",
+                    "LIBOG Félix Eboué Amos": {
+                        "LIBOG Jean Joseph": null,
+                        "LIBOG Laetitia Laure Lea": null
+                    },
+                }
+            },
+            "NGO DJOB dite NGO KOMBE": {
+                "TUK New Sadrack": null,
+                "JOB Li TUK": null
+            },
+            "BIDJECK BI JOB": null
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    let li0 = document.createElement("li"),
+        div0 = document.createElement("div"),
+        sp0 = document.createElement("span"),
+        ul0 = document.createElement("ul");
+
+    document.getElementById("famille").append(ul0);
+
+    // coloration des ascendants
+    const col = ind => {
+        ind.addEventListener("mouseover", () => {
+            for (i = 3; i < ind.id.length + 1; i++) {
+                document.getElementById(ind.id.substring(0, i)).classList.add("red");
+            }
+        })
+        ind.addEventListener("mouseout", () => {
+            for (i = 3; i < ind.id.length + 1; i++) {
+                document.getElementById(ind.id.substring(0, i)).classList.remove("red");
+            }
+        })
+    };
+
+    // création de l'arbre dom
+    const run = (t, ref, pre) => {
+        // pour les ancêtres
+        if (typeof t == "object") {
+            let num = 0;
+            for (let i in t) {
+                num++;
+                let li = li0.cloneNode(),
+                    div = div0.cloneNode(),
+                    sp = sp0.cloneNode();
+                sp.textContent = i;
+
+                // masquage / affichage des descendants
+                sp.addEventListener("click", (e) => {
+                    if (e.target.parentElement.nextSibling) {
+                        e.target.parentElement.nextSibling.classList.toggle("cache");
+                        if (sp.textContent.indexOf("+") != -1) {
+                            sp.textContent = sp.textContent.replace("+", "-")
+                        }
+                        else {
+                            sp.textContent = sp.textContent.replace("-", "+")
+                        }
+                    }
+                });
+                ref.append(li);
+                li.append(div);
+                sp.id = "id" + pre + num;
+                div.append(sp);
+                col(sp)
+
+                // membres avec enfants
+                if (t[i] != null) {
+                    let ul = ul0.cloneNode();
+                    ul.classList.add("cache");
+                    li.append(ul);
+                    sp.textContent += " +";
+                    sp.classList.add("pointer");
+                    run(t[i], ul, pre + num)
+                }
+            }
+        }
+        // membres sans enfant
+        else {
+            let li = li0.cloneNode(),
+                div = div0.cloneNode(),
+                sp = sp0.cloneNode();
+            sp.textContent = t;
+            sp.id = "id" + pre + 1;
+            ref.append(li);
+            li.append(div);
+            div.append(sp);
+            col(sp)
+        }
+    }
+
+    run(tab, document.querySelector("#famille ul"), "")
+    document.querySelectorAll("#famille ul")[0].classList.remove("cache");
+})
